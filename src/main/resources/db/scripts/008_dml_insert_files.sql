6161d74d7ea3a83f8305236cd8b3da4c03d13800
Shaman2k <103325541+Shaman2k@users.noreply.github.com>
2025-06-04 18:16:47 +0300

    Create DB schema, liquidbase scripts, test data

---
+++ src/main/resources/db/scripts/008_dml_insert_files.sql
@@ -0,0 +1,6 @@
+INSERT INTO files (name, path) VALUES
+                                   ('Аватар', '/posters/avatar.jpg'),
+                                   ('Интерстеллар', '/posters/interstellar.jpg'),
+                                   ('Матрица', '/posters/matrix.jpg'),
+                                   ('Крестный отец', '/posters/godfather.jpg'),
+                                   ('Побег из Шоушенка', '/posters/shawshank.jpg');
---
+++ src/main/resources/db/scripts/011_dml_insert_halls.sql
@@ -0,0 +1,3 @@
+INSERT INTO halls (name, row_count, place_count, description) VALUES
+                                                                  ('Красный зал', 10, 20, 'Основной зал с цифровым проектором'),
+                                                                  ('Синий зал', 8, 15, 'Малый зал с 3D оборудованием');
