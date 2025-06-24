c4aee2a1d254e4b11e6cb9077fa8d6f77725ab50
Shaman2k <103325541+Shaman2k@users.noreply.github.com>
2025-06-24 13:35:19 +0300

    User controller

---
+++ src/main/java/ru/job4j/cinema/controller/UserController.java
@@ -0,0 +1,71 @@
+package ru.job4j.cinema.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.job4j.cinema.model.User;
+import ru.job4j.cinema.service.UserService;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/register")
+    public String getRegistrationPage(Model model, HttpSession session) {
+        addUserToModel(model, session);
+        return "users/register";
+    }
+
+    @PostMapping("/register")
+    public String register(Model model, @ModelAttribute User user) {
+        var userOptional = userService.save(user);
+        if (userOptional.isEmpty()) {
+            model.addAttribute("error", "Пользователь с таким email уже сужествует");
+            return "users/register";
+        }
+        return "redirect:/sessions";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage(Model model, HttpSession session) {
+        addUserToModel(model, session);
+        return "users/login";
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@ModelAttribute User user, Model model, HttpServletRequest request) {
+        var userOptional = userService.findByEmailAndPassword(user.getEmail(), user.getPassword());
+        if (userOptional.isEmpty()) {
+            model.addAttribute("error", "Почта или пароль введены неверно");
+            return "users/login";
+        }
+        var session = request.getSession();
+        session.setAttribute("user", userOptional.get());
+        return "redirect:/sessions";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/users/login";
+    }
+
+    private void addUserToModel(Model model, HttpSession session) {
+        var user = (User) session.getAttribute("user");
+        if (user == null) {
+            user = new User();
+            user.setFullName("Гость");
+        }
+        model.addAttribute("user", user);
+    }
+}
---
+++ src/main/java/ru/job4j/cinema/repository/Sql2oUserRepository.java
@@ -0,0 +1,54 @@
+package ru.job4j.cinema.repository;
+
+import org.springframework.stereotype.Repository;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+import ru.job4j.cinema.model.User;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Repository
+public class Sql2oUserRepository implements UserRepository {
+    private static final Logger LOG = LoggerFactory.getLogger(Sql2oUserRepository.class.getName());
+    private final Sql2o sql2o;
+
+    public Sql2oUserRepository(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public Optional<User> save(User user) {
+        try (var connection = sql2o.open()) {
+            var sql = """
+                    INSERT INTO users(email, full_name, password)
+                    VALUES (:email, :fullName, :password)
+                    """;
+
+            var query = connection.createQuery(sql, true)
+                    .addParameter("email", user.getEmail())
+                    .addParameter("fullName", user.getFullName())
+                    .addParameter("password", user.getPassword());
+            int generatedId = query.executeUpdate().getKey(Integer.class);
+            user.setId(generatedId);
+            return Optional.of(user);
+        } catch (Sql2oException e) {
+            LOG.error("Ошибка при сохранении пользователя {}", user.getEmail(), e);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> findByEmailAndPassword(String email, String password) {
+        try (var connection = sql2o.open()) {
+            var query = connection.createQuery("SELECT * FROM users WHERE email = :email AND password = :password")
+                    .addParameter("email", email)
+                    .addParameter("password", password);
+            var user = query.setColumnMappings(User.COLUMN_MAPPING).executeAndFetchFirst(User.class);
+            return Optional.ofNullable(user);
+        }
+
+    }
+}
